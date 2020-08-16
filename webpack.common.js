12f83c35a43089dfd60915d6f9dbfe1e667c3612
shivaweb03 <shiva03@outlook.com>
2020-08-16 21:16:49 +0530

    html loader,fileloader and clean code to remove auto genereated files

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,20 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(svg|jpg|jpeg|png|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'imgs'
+                    }
+                }
             }
         ]
     },
